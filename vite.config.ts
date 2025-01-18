16181150b5e2cecaef65eed1a0fedc236e4254c5
Matt Hensen <matthew.hensen92@gmail.com>
2025-01-17 19:33:37 -0600

    added tailwind, shadcn, and basic testing suite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+/// <reference types="vitest/config" />
+import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    environment: 'happy-dom',
+    coverage: {
+      reporter: ['default', 'json', 'html'],
+    },
+  },
 });
