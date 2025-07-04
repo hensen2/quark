b2a39f4af63c4268dfef6afec768ae466da45f2f
Matt Hensen <hensen.dev@gmail.com>
2025-07-04 14:46:35 -0500

    added all shadcn components

--- packages/ui/src/components/primitives/sonner.tsx
+++ packages/ui/src/components/primitives/sonner.tsx
@@ -1,5 +1,4 @@
 import { useTheme } from 'next-themes';
-import type * as React from 'react';
 import { Toaster as Sonner, type ToasterProps } from 'sonner';
 
 const Toaster = ({ ...props }: ToasterProps) => {
